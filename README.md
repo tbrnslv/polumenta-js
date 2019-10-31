934b2e6309c8253fa9487a2e06f80567906caf30
tbrnslv <tbrnslv@gmail.com>
2019-10-31 03:03:40 +0100

    added neutral gender

--- README.md
+++ README.md
@@ -2,13 +2,11 @@
 
 Inspired and originally forked from https://github.com/jablan/polumenta
 
-
 On NPM:
 
 npm install polumenta-js
 
 yarn add polumenta-js
-
 
 with ES6 imports:
 import getPolumenta from 'polumenta-js';
@@ -16,4 +14,4 @@ import getPolumenta from 'polumenta-js';
 or with ES5:
 var getPolumenta = require('polumenta-js')
 
-getPolumenta function accepts two optional parameters - script ('latin'(default) or 'cyrilic') and gender ('male'(default) or 'female')
+getPolumenta function accepts two optional parameters - script ('latin'(default) or 'cyrilic') and gender ('male'(default), 'neutral' or 'female')
--- index.js
+++ index.js
@@ -5,26 +5,33 @@ const getRandomInt = max => {
   return Math.floor(Math.random() * (max - 0));
 };
 
+const getGenderEnding = (gender, choosenScript) => {
+  switch (gender) {
+    case "male":
+      return letters[choosenScript].d[0];
+    case "neutral":
+      return letters[choosenScript].d[1];
+    case "female":
+      return letters[choosenScript].d[2];
+    default:
+      return letters[choosenScript].d[0];
+  }
+};
+
 const getPolumenta = (script, gender) => {
   const choosenScript = script || "latin";
   const choosenGender = gender || "male";
   let currentPolumenta = "";
-  currentPolumenta +=
-    letters[choosenScript].a[getRandomInt(letters[choosenScript].a.length)];
-  currentPolumenta +=
-    letters[choosenScript].b[getRandomInt(letters[choosenScript].b.length)];
-  currentPolumenta +=
-    letters[choosenScript].c[getRandomInt(letters[choosenScript].c.length)];
-  return (currentPolumenta +=
-    choosenGender === "male"
-      ? letters[choosenScript].d[0]
-      : letters[choosenScript].d[1]);
+  currentPolumenta += letters[choosenScript].a[getRandomInt(letters[choosenScript].a.length)];
+  currentPolumenta += letters[choosenScript].b[getRandomInt(letters[choosenScript].b.length)];
+  currentPolumenta += letters[choosenScript].c[getRandomInt(letters[choosenScript].c.length)];
+  return (currentPolumenta += getGenderEnding(choosenGender, choosenScript));
 };
 
 /**
  * Generate Polumenta
  * @param {string} script Script - 'latin' or 'cyrilic', default is 'latin' (this parameter changes script of Polumenta's name)
- * @param {string} gender Gender - 'male' or 'female', default is 'male' (this parameter changes last letter of Polumenta's name)
+ * @param {string} gender Gender - 'male', 'neutral' or 'female', default is 'male' (this parameter changes last letter of Polumenta's name)
  * @return {string}
  */
 module.exports = (script, gender) => {
